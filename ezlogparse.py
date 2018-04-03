b4f279107a31564aafc675d20b2eb25ce28afc33
Norman Roy <32035641+nmdeguia@users.noreply.github.com>
2018-04-03 20:40:46 +0800

    Add files via upload

--- ezlogparse.py
+++ ezlogparse.py
@@ -7,19 +7,20 @@ Created on Tue Apr  3 16:18:35 2018
     Norman Roy de Guia
     Al Tristan Bandiola
 """
+from collections import Counter
 import sys;
 
 def main(filename, csvfile):
     #filename = "sample_data";
     #csvfile = "parsed_data.csv";
-    print("Filename is:", filename);
+    print("Parsing %s." % filename);
     
     # Open data file and store in data vector.
     data = open(filename, 'r');
     data.seek(0);
     data_count = len(data.readlines());
     data.seek(0);
-    print("Total lines in dataset is", data_count);
+    print("Total lines in dataset is  %d." % data_count);
     data.seek(0);
     
     data_arr = list(data);
@@ -27,12 +28,25 @@ def main(filename, csvfile):
     
     data.close(); # close file
     
+    # parse data and store to parsed_data
+    parsed_data = parse_data(data_arr, data_count);
+
+    # dump contents to csv file
+    csvdump = open(csvfile, "w");
+    csvdumpstr = ''.join(parsed_data);
+    csvdump.write(csvdumpstr);
+    print("Parsing complete, output dump: %s." % csvfile);
+
+    # generate statistical report on data
+    get_statistics(parsed_data);
+
+def parse_data(data, datalen):
     # find lookup string and isolate
     lookup = '.pdf';
     parse_arr = [];
-    for x in range(data_count):
-        if lookup in data_arr[x][6]:
-            parse_arr.append(data_arr[x]);
+    for x in range(datalen):
+        if lookup in data[x][6]:
+            parse_arr.append(data[x]);
             
     ipaddr = [];
     name = [];
@@ -40,7 +54,7 @@ def main(filename, csvfile):
     tzone = [];
     request = [];
     parse_arr_count = len(parse_arr);
-    print("Total valuable lines:", parse_arr_count);
+    print("Total valuable lines: %d" % parse_arr_count);
     
     final_arr = [0 for x in range(parse_arr_count)];
     
@@ -58,11 +72,19 @@ def main(filename, csvfile):
         temp += tzone[x] + ", ";
         temp += request[x] + "\n";
         final_arr[x] = temp;
-        
-    # dump contents to csv file
-    csvdump = open(csvfile, "w");
-    csvdumpstr = ''.join(final_arr);
-    csvdump.write(csvdumpstr);
+
+    return final_arr;
+
+def get_statistics(data):
+    datalen = len(data);
+
+    print("----------------------------------------");
+    print("Statistical Report for acquired dataset:");
+
+    unique_data = Counter(data);
+    n_unq_data = len(unique_data);
+    print("Number of unique items: %d" % n_unq_data);
+
 
 if __name__ == "__main__": #main();
     main(sys.argv[1], sys.argv[2]);
