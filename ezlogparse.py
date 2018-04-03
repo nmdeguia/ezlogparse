133f34adfef3a656106e11e62851146c41140559
Norman Roy <32035641+nmdeguia@users.noreply.github.com>
2018-04-03 17:47:03 +0800

    Add files via upload

--- ezlogparse.py
+++ ezlogparse.py
@@ -5,39 +5,47 @@ Created on Tue Apr  3 16:18:35 2018
 
 @author: norman
 """
-#import sys;
+import sys;
 
-#def main():
-filename = "sample_data"
-print("filename is:", filename);
+def main(filename):
+    #filename = "sample_data"
+    print("filename is:", filename);
+    
+    # Open data file and store in data vector.
+    data = open(filename, 'r');
+    data.seek(0);
+    data_count = len(data.readlines());
+    data.seek(0);
+    print(data.read());
+    print("Total lines in dataset is", data_count);
+    data.seek(0);
+    
+    data_arr = list(data);
+    data_arr = [i.split(' ') for i in data_arr];
+    
+    data.close();
+    
+    # find lookup string and isolate
+    lookup = '.pdf';
+    final_arr = [];
+    for i in range(data_count):
+        if lookup in data_arr[i][6]:
+            final_arr.append(data_arr[i]);
+            
+    ip = [];
+    name = [];
+    date = [];
+    tzone = [];
+    request = [];
+    final_arr_count = len(final_arr);
+    
+    # parse and store needed data
+    for x in range(final_arr_count):
+        ip.append(final_arr[x][0]);
+        name.append(final_arr[x][2]);
+        date.append(final_arr[x][3].strip("["));
+        tzone.append(final_arr[x][4].strip("]"));
+        request.append(final_arr[x][6]);
 
-# Open data file and store in data vector.
-data = open(filename, 'r');
-data.seek(0);
-data_count = len(data.readlines());
-data.seek(0);
-print(data.read());
-print("Total lines in dataset is", data_count);
-data.seek(0);
-
-data_arr = [0 for x in range(data_count)];
-for x in range(data_count):
-    data_arr[x] = (data.readline()).split(" ");
-
-data.close();
-
-lookup = ".pdf";
-final_arr = [];
-for i in range(data_count):
-    if lookup in data_arr[i][6]:
-        final_arr.append(data_arr[i]);
-        
-ip_arr = [];
-final_arr_count = len(final_arr);
-split_count = 10;
-for x in range(final_arr_count):
-    print("count");
-    ip_arr.append(final_arr[x][0]);
-
-#if __name__ == "__main__": main();
-    #main(sys.argv[1]);
+if __name__ == "__main__": #main();
+    main(sys.argv[1]);
