0ab039e2ebf67adac2cb82fc2014e9d5758fbef1
Norman Roy <32035641+nmdeguia@users.noreply.github.com>
2018-04-10 14:03:10 +0800

    Add files via upload

--- ezlogparse.py
+++ ezlogparse.py
@@ -1,142 +1,112 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr  3 16:18:35 2018
+from sys import argv
 
-@authors:
-    Norman Roy de Guia
-    Al Tristan Bandiola
-"""
-import sys;
-from collections import Counter;
+script, in_file, out_file = argv
 
-ipaddr = [];
-name = [];
-date = [];
-taccess = [];
-tzone = [];
-request = [];
+def niceprint(list):
+	print '--------------------' #20
+	for i in list:
+		print i
+	print '------------------------------------------------------------' #60
+	return
 
-# debug string for debugging purposes
-debug = "debug string";
+def strprintln(string):
+	print '++++++++++++++++++++' #20
+	print string.split('\n')
+	print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' #60
+	return
+	
+class EZlog(object):
 
-# in minutes
-timewindow = 1440/24/60;
+	def __init__(self, in_file):
+		self.in_file = open(in_file, 'r')
+		self.raw = list(self.in_file)
+		self.in_file.close()
 
-def main(filename, csvfile):
-    #filename = "sample_data";
-    #csvfile = "parsed_data.csv";
-    print("Parsing %s..." % filename);
-    
-    # Open data file and store in data vector.
-    data = open(filename, 'r');
-    data.seek(0);
-    data_count = len(data.readlines());
-    data.seek(0);
-    print("Total lines in dataset is  %d." % data_count);
-    data.seek(0);
-    
-    data_arr = list(data);
-    data_arr = [i.split(' ') for i in data_arr];
-    
-    data.close(); # close file
-    
-    # parse data and store to parsed_data
-    parsed_data = parse_data(data_arr, data_count);
+		with open(in_file, 'r') as f:
+			self.str_split = [i.split() for i in f]	#used in search function
+		
+		self.lookup = list()	#used in search function
+		#self.gen_lookup = list()
+		
+		self.ip = list()		#0
+		self.name = list()		#2
+		self.date = list()		#3[
+		self.tzone = list()		#4]
+		self.request = list()	#6
+		self.csv_string = ""
+		self.parsed = list()
+		
+	def search(self, keyword):
+		#self.lookup = list()	#reinitialize lookup list
+		for i in range(len(self.str_split)):
+			if keyword in self.str_split[i][0]:			#search within ip
+				self.lookup.append(self.str_split[i])
+		for i in range(len(self.str_split)):
+			if keyword in self.str_split[i][2]:			#search within name
+				self.lookup.append(self.str_split[i])
+		for i in range(len(self.str_split)):
+			if keyword in self.str_split[i][3]:			#search within date
+				self.lookup.append(self.str_split[i])
+		for i in range(len(self.str_split)):
+			if keyword in self.str_split[i][4]:			#search within tzone
+				self.lookup.append(self.str_split[i])
+		for i in range(len(self.str_split)):
+			if keyword in self.str_split[i][6]:			#search within request
+				self.lookup.append(self.str_split[i])
+		return self.lookup
+		
+	def extract(self):
+		for i in range(len(self.lookup)):
+			self.ip.append(self.lookup[i][0])
+			self.name.append(self.lookup[i][2])
+			self.date.append(self.lookup[i][3].strip('['))
+			self.tzone.append(self.lookup[i][4].strip(']'))
+			self.request.append(self.lookup[i][6])
+			self.csv_string += self.ip[i] + ', '
+			self.csv_string += self.name[i] + ', '
+			self.csv_string += self.date[i] + ', '
+			self.csv_string += self.tzone[i] + ', '
+			self.csv_string += self.request[i] + '\n'
+			self.parsed = self.csv_string
+		return self.parsed
+	
+	def csvdump(self):
+		self.out_file = open(out_file, 'w')
+		self.out_file.write(self.parsed)
+		self.out_file.close()
+			
+	def csvappend(self):
+		self.out_file = open(out_file, 'a')
+		self.out_file.write(self.parsed)
+		self.out_file.close()
+			
+	def analyze(self):
+		pass
+			
+a = EZlog(in_file)
+b = EZlog(in_file)
+c = EZlog(in_file)
 
-    # dump contents to csv file
-    csvdump = open(csvfile, "w");
-    csvdumpstr = ''.join(parsed_data);
-    csvdump.write(csvdumpstr);
-    print("Parsing complete, output dump: %s." % csvfile);
+print 'this is raw of a'
+#niceprint(a.raw)
 
-    # parse date into more meaningful data
-    parse_date();
+print 'this is lookup of a'
+a.search('.pdf')
+#niceprint(a.search('.pdf'))
 
-    # generate statistical report on data
-    get_statistics(csvdumpstr);
+#b.search('BARTON')
+#c.search('86.55.237.139')
 
-def parse_data(data, datalen):
-    # find lookup string and isolate
-    lookup = '.pdf';
-    parse_arr = [];
-    for x in range(datalen):
-        if lookup in data[x][6]:
-            parse_arr.append(data[x]);
-            
-    parse_arr_count = len(parse_arr);
-    print("Total valuable lines: %d" % parse_arr_count);
-    
-    final_arr = [0 for x in range(parse_arr_count)];
-    
-    # parse and store needed data
-    for x in range(parse_arr_count):
-        temp = "";
-        ipaddr.append(parse_arr[x][0]);
-        name.append(parse_arr[x][2]);
-        taccess.append(parse_arr[x][3].strip("["));
-        tzone.append(parse_arr[x][4].strip("]"));
-        request.append(parse_arr[x][6]);
-        temp += ipaddr[x] + ", ";
-        temp += name[x] + ", ";
-        temp += taccess[x] + ", ";
-        temp += tzone[x] + ", ";
-        temp += request[x] + "\n";
-        final_arr[x] = temp;
+#print 'this is str_split of a'
+#niceprint(a.str_split)
 
-    return final_arr;
+a.extract()
+#b.extract()
+#c.extract()
 
-def get_statistics(data):
-    print("----------------------------------------");
-    print("Statistical Report for acquired dataset:");
-    temp = [];
-    temp = data.split("\n")[:-1]
+#print 'this is csv_string of a'
+#print a.csv_string
 
-    print("Total valuable items: %d " % len(temp));
-    items = [0 for x in range(len(temp))];
-    for x in range(len(temp)):
-        items[x] = temp[x].split(", ");
-
-    get_items_per_timewindow(items);
-
-def get_items_per_timewindow(items):
-    print("Time Slice of %d minutes." % (timewindow*24*60));
-    print("%d items to slice." % len(items));
-    print("%d individual time records." % len(date));
-
-    # compare date here:
-    unique_time_slices = 1;
-    unique_tslice_index = [0];
-    for x in range(len(items)):
-        if x + 1 == len(items):
-            break;
-        # check if year is same as next item    
-        if date[x][2] == date[x+1][2]: 
-            if date[x][1] == date[x+1][1]:
-                # only works for 1 day time slice
-                # change code if timewindow < 1 day is needed
-                if date[x][0] == date[x+1][0]:
-                    pass;
-                else:
-                    # increment counter of unique slices
-                    unique_time_slices += 1;
-                    # get starting index of time slice window
-                    unique_tslice_index.append(x+1);
-
-    print("%d unique time slices." % unique_time_slices)
-    print("Indices of unique time slices: %s" % unique_tslice_index)
-    
-    # we now have indices of time slices
-    # locate index and iterate up to next index - 1
-    # count number of same request occurences
-
-def parse_date():
-    temp = [];
-    # print(isinstance(date, list));
-    for x in range(len(taccess)):
-        temp = taccess[x][:11].split('/');
-        temp += taccess[x][12:].split(':');
-        date.append(temp);
-
-if __name__ == "__main__": #main();
-    main(sys.argv[1], sys.argv[2]);
+a.csvdump()
+#b.csvappend()
+#c.csvappend()
