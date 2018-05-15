967d9ef68adb707c9fc3caba8bba696c4a7b29d7
nmdeguia <normanroydeguia@gmail.com>
2018-05-15 20:23:45 +0800

    fixed indentation errors

--- src/ezstat.py
+++ src/ezstat.py
@@ -38,61 +38,61 @@ def generate(args, global_data, items, flag):
 	else: mode = 'a'
 
 	for iter, x in enumerate(range(timeslices),1):
-	if (verbose): print('--------------------------------------------------')
+		if (verbose): print('--------------------------------------------------')
 
-	items.string.append('Timeslice no. {0} ({1} - {2})'.format(
-		iter, basetime, basetime+timewindow))
-	if (verbose): print(items.string[-1])
+		items.string.append('Timeslice no. {0} ({1} - {2})'.format(
+			iter, basetime, basetime+timewindow))
+		if (verbose): print(items.string[-1])
 
-	# initialize time slice indices
-	uppertime = basetime + timewindow
-	baseindex = items.locate_index(basetime)
+		# initialize time slice indices
+		uppertime = basetime + timewindow
+		baseindex = items.locate_index(basetime)
 
-	# set ceiling value for uppertime
-	if uppertime >= items.unixtime[-1]: uppertime = items.unixtime[-1]
-	upperindex = items.locate_index(uppertime)
+		# set ceiling value for uppertime
+		if uppertime >= items.unixtime[-1]: uppertime = items.unixtime[-1]
+		upperindex = items.locate_index(uppertime)
 
-	if upperindex != baseindex:
-		#length = len(items.unixtime[baseindex:upperindex])
-		if (x != timeslices-1): upperindex -= 1
-		else: upperindex = items.locate_index(uppertime)
-	# else: length = 0
+		if upperindex != baseindex:
+			#length = len(items.unixtime[baseindex:upperindex])
+			if (x != timeslices-1): upperindex -= 1
+			else: upperindex = items.locate_index(uppertime)
+		# else: length = 0
 
-	# get unixtime value of upperbound and lowerbound indices
-	baseindexvalue = items.unixtime[baseindex]
-	upperindexvalue = items.unixtime[upperindex]
+		# get unixtime value of upperbound and lowerbound indices
+		baseindexvalue = items.unixtime[baseindex]
+		upperindexvalue = items.unixtime[upperindex]
 
-	items.string.append('{0} to {1}'.format(
-		datetime.datetime.fromtimestamp(
-			int(baseindexvalue)).strftime('%Y-%m-%d %H:%M:%S'),
-		datetime.datetime.fromtimestamp(
-			int(upperindexvalue)).strftime('%Y-%m-%d %H:%M:%S')
-		))
-	if (verbose): print(items.string[-1])
-	items.string.append('Base: {0} [{1}], Upper: {2} [{3}]'.format(
-		baseindexvalue, baseindex, upperindexvalue, upperindex))
-	if (verbose): print(items.string[-1])
-	# items.string.append('Number of items in sublist: {0}'.format(length))
-	# if (verbose): print(items.string[-1])
+		items.string.append('{0} to {1}'.format(
+			datetime.datetime.fromtimestamp(
+				int(baseindexvalue)).strftime('%Y-%m-%d %H:%M:%S'),
+			datetime.datetime.fromtimestamp(
+				int(upperindexvalue)).strftime('%Y-%m-%d %H:%M:%S')
+			))
+		if (verbose): print(items.string[-1])
+		items.string.append('Base: {0} [{1}], Upper: {2} [{3}]'.format(
+			baseindexvalue, baseindex, upperindexvalue, upperindex))
+		if (verbose): print(items.string[-1])
+		# items.string.append('Number of items in sublist: {0}'.format(length))
+		# if (verbose): print(items.string[-1])
 
-	# statistical function generation starts here
-	unique = items.get_unique_content(baseindex, upperindex)
-	on_conn, off_conn = cnt_oncampus_requests(unique, oncampaddr, items.string)
+		# statistical function generation starts here
+		unique = items.get_unique_content(baseindex, upperindex)
+		on_conn, off_conn = cnt_oncampus_requests(unique, oncampaddr, items.string)
 
-	# get total number of unique items per logfile
-	if (iter == 1):
-		unique_items = len(unique)
-		unique_on_conn = on_conn
-		unique_off_conn = off_conn
-	else:
-		unique_items += len(unique)
-		unique_on_conn += on_conn
-		unique_off_conn += off_conn
+		# get total number of unique items per logfile
+		if (iter == 1):
+			unique_items = len(unique)
+			unique_on_conn = on_conn
+			unique_off_conn = off_conn
+		else:
+			unique_items += len(unique)
+			unique_on_conn += on_conn
+			unique_off_conn += off_conn
 
-	# checks if timeslice is the last one
-	# ends loop if timeslice reaches EOL
-	if x == timeslices-1: break
-	else: basetime = uppertime
+		# checks if timeslice is the last one
+		# ends loop if timeslice reaches EOL
+		if x == timeslices-1: break
+		else: basetime = uppertime
 
 	items.finalize()
 	common_sites = items.get_unique_sites()	#[0] - site, [1] - frequency
@@ -119,11 +119,11 @@ def cnt_oncampus_requests(data, oncampaddr, strings):
 	off_campus_count = 0
 	unicode_ip_net = str(oncampaddr)
 	for i in range(len(data)):
-	unicode_ip_request = str(data[i][0])
-	if ipaddress.ip_address(
-		unicode_ip_request) in ipaddress.ip_network(unicode_ip_net):
-		on_campus_count += 1
-	else: off_campus_count += 1
+		unicode_ip_request = str(data[i][0])
+		if ipaddress.ip_address(
+			unicode_ip_request) in ipaddress.ip_network(unicode_ip_net):
+			on_campus_count += 1
+		else: off_campus_count += 1
 	strings.append('Number of on-campus accesses: {0}'.format(on_campus_count))
 	if (verbose): print(strings[-1])
 	strings.append('Number of off-campus accesses: {0}'.format(off_campus_count))
